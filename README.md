2e5dee38095c26b038bf717e49180be529724824
Vivek Naskar <viveknaskar2912@gmail.com>
2021-07-19 02:41:31 +0530

    Created a class demonstrating a volatile keyword

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ For implementing multithreading, there are two ways in which threads can be crea
 1. By implementing the Runnable interface
 2. By extending the Thread class
 
-### Things to know
+## Things to know
 1. The start() method creates a new thread that executes the run() method.
+   
 2. The run() method just executes in the current thread, without starting a new thread.
+   
+3. volatile keyword is used to make classes thread safe. The keyword does not cache the value 
+   of the variable but instead, it reads the variable from the main memory. It is used to modify 
+   the value of a variable by different threads.
+   
+
---
+++ src/com/viveknaskar/VolatileKeywordDemo.java
@@ -0,0 +1,45 @@
+package com.viveknaskar;
+import java.util.Scanner;
+
+class Processor extends Thread {
+
+    /**
+     * Volatile is used for preventing Threads caching variable when they are not changed
+     * within that thread.
+     *
+     */
+    private volatile boolean running = true;
+
+    public void run() {
+
+        while (running) {
+            System.out.println("hello there");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdown() {
+        running = false;
+    }
+
+}
+
+public class VolatileKeywordDemo {
+
+    public static void main(String[] args) {
+
+        Processor processorOne = new Processor();
+        processorOne.start();
+
+        System.out.println("Press return to shutdown...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        processorOne.shutdown();
+
+    }
+}
