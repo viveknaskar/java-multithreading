d1dbd5947fdbed00a19a6a451ef96885106506a0
Vivek Naskar <viveknaskar2912@gmail.com>
2021-08-01 20:53:43 +0530

    Illustration of Interrupting Threads

---
+++ src/com/viveknaskar/interruptingthreads/InterruptingThreadsDemo.java
@@ -0,0 +1,31 @@
+package com.viveknaskar.interruptingthreads;
+
+import java.util.Random;
+
+public class InterruptingThreadsDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Starting...");
+        Thread thread1 = new Thread(() -> {
+            Random random = new Random();
+
+            for(int i=0; i<1E8; i++) {
+                if(Thread.currentThread().isInterrupted()) {
+                    System.out.println("Interrupted!!!");
+                    break;
+                }
+                Math.sin(random.nextDouble());
+            }
+        });
+        thread1.start();
+
+        Thread.sleep(500);
+
+        thread1.interrupt();
+        thread1.join();
+
+        System.out.println("Finished!");
+
+    }
+}
